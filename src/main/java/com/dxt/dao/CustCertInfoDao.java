51512fa09c0b8d85bdb561b993cbeafddf430a49
liuhonghao1994 <1173111672@qq.com>
2020-04-21 10:16:00 +0800

    更改相关接口

--- src/main/java/com/dxt/dao/CustCertInfoDao.java
+++ src/main/java/com/dxt/dao/CustCertInfoDao.java
@@ -17,7 +17,7 @@ public interface CustCertInfoDao {
             @Result(property = "gender", column = "gender")
     })
     CustCertInfo getCustInfoByIdCodeAndName(@Param("idCode") String idCode,
-                                                   @Param("name") String name);
+                                            @Param("name") String name);
 
     @Select("select t.id,t.name,t.idcode,t.birthday,t.gender from dxtcharge.oip_idinfo t" +
             " where  t.idcode = #{idCode,jdbcType=VARCHAR}")
@@ -34,7 +34,7 @@ public interface CustCertInfoDao {
             "select #{id}," +
             "#{name},#{idCode},substr(#{idCode},7,8)," +
             "decode(mod(substr(#{idCode},17,1),2),0,'女',1,'男'," +
-            "mod(substr(#{idCode},17,1),2)) from dual a where " +
+            "mod(substr(#{idCode},17,1),2)) from dual a where  not exists " +
             "(select 1 from dxtcharge.oip_idinfo a where a.idcode = #{idCode} and a.name = #{name})")
     void insertCustCertInfo(CustCertInfo info);
 
